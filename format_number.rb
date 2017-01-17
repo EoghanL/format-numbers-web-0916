3478ee8718ce1eba957b7464ed074c352b537dee
github username <github email address>
2017-01-17 15:49:08 -0500

    all green

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,10 @@
 def separate_with_comma(num)
-  # code goes here
+  num = num.to_s
+  num if num.length <= 3
+  remainder = num.length % 3
+  while num.length > remainder
+    num.insert(remainder, ',') if remainder != 0
+    remainder == 0 ? remainder += 3 : remainder += 4
+  end
+  num
 end
